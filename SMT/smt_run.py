53cc6f453839431ffd8c03c130c5f97f6d0bc64b
CacioCavalloIsNotReal <francescocavaleri8991@gmail.com>
2025-05-25 17:26:52 +0200

    when all it evaluates bot sb and not sb

--- SMT/smt_run.py
+++ SMT/smt_run.py
@@ -50,6 +50,7 @@ def execute_smt(symbreak: bool = False, instance_name: str = "all"):
 
     module_path = os.path.dirname(os.path.realpath(__file__))
     outpath = "/".join(module_path.split('/')[:-1]+['res', 'SMT'])
+    os.makedirs(outpath, exist_ok=True)
 
     instances_folder =  "/".join(module_path.split('/')[:-1]+['instances'])
     file_names = sorted(os.listdir(instances_folder) )
@@ -57,24 +58,30 @@ def execute_smt(symbreak: bool = False, instance_name: str = "all"):
         print("Running on all instances...")
         pbar = tqdm(file_names)
         for name in pbar:
-            pbar.set_description(f"solving problem {name}")
-            path=instances_folder+f'/{name}'
-            filename,m,n,l,s,d = read_raw_instances(path)
-            result_dict = run_z3_with_external_timeout(
-                external_timeout_seconds=MAX_TIME,
-                model_func=my_model,
-                m=m, 
-                n=n, 
-                l=l, 
-                s=s, 
-                d=d,
-                symm_break=symbreak, # Prova con True o False
-                timeout=MAX_TIME # Timeout interno per Z3 (in ms)
-            )
-            if symbreak:
-                write_output(prepare_solution(result_dict), f'./SMT/result_symbreak/{filename}.json')
-            else:
-                write_output(prepare_solution(result_dict), f'./SMT/result_nosymbreak/{filename}.json')
+            for sb in [True, False]:
+                pbar.set_description(f"solving problem {name}")
+                path=instances_folder+f'/{name}'
+                filename,m,n,l,s,d = read_raw_instances(path)
+                result_dict = run_z3_with_external_timeout(
+                    external_timeout_seconds=MAX_TIME,
+                    model_func=my_model,
+                    m=m, 
+                    n=n, 
+                    l=l, 
+                    s=s, 
+                    d=d,
+                    symm_break=sb, # Prova con True o False
+                    timeout=MAX_TIME # Timeout interno per Z3 (in ms)
+                )
+                if sb:
+                    write_output(prepare_solution(result_dict), f'./SMT/result_symbreak/{filename}.json')
+                else:
+                    write_output(prepare_solution(result_dict), f'./SMT/result_nosymbreak/{filename}.json')
+
+                combine_results(
+                    result_nosymbreak_dir="./SMT/result_nosymbreak",
+                    result_symbreak_dir="./SMT/result_symbreak"
+                )
 
     else:
         instance_name = choose_instance(instance_name)
